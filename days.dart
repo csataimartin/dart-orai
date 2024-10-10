321a76c3d8d3b85265ba139c9cf6ece1e6c3e11b
Csatai Martin <szt3csamar@vasvari.org>
2024-10-10 15:28:47 +0200

    Add files via upload

---
+++ days.dart
@@ -0,0 +1,38 @@
+void main(){
+  
+  Set <String> week = {"Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"};
+  print(week);
+  print("Az első elem: ${week.first}");
+  print("Az utolsó elem: ${week.last}");
+  print("Üres-e? ${week.isEmpty}");
+  print("Nem üres-e? ${week.isNotEmpty}");
+  print("A hossza? ${week.length}");
+  print("Tartalmazza-e a Szerda értéket? ${week.contains("Szerda")}");
+  print("Tartalmazza-e a Wednesday értéket? ${week.contains("Wednesday")}");
+
+
+  Set<String> asd = {"workday", "weekend", "holiday", "public holiday", "bank holiday", "national holiday", "religious holiday", "federal holiday", "school holiday", 
+                      "company holiday", "floating holiday", "seasonal holiday", "observed holiday", "half-day",
+                       "flexible day", "personal day", "sick day", "leave of absence", "vacation day", "remote workday"};
+  for (String i  in asd)
+  {
+    week.add(i);
+  }
+  for(String teljes in week){
+   print(teljes);
+ }
+   Set<String> schoolDays = {};
+
+   for (String i  in week)
+  {
+    if(i.contains("school"))
+    {
+      schoolDays.add(i);
+    }
+
+    
+  }
+
+  print("Az iskolai nappok: ${schoolDays}");
+
+}
